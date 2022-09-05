2f54cb46b0c34fa5c8d745b7f15f693a0a5105ba
Saurav Kumar <kumarsaurav26198@gmail.com>
2022-09-05 14:48:43 +0530

    crud with mongodb done

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dbConnect = require('./mongodb');
 const app = express();
+const mongodb = require('mongodb');
 
 app.use(express.json());
 app.get('/', async (req, resp) => {
@@ -23,6 +24,13 @@ app.put("/:phoneNumber", async (req, resp) => {
         { phoneNumber: req.params.phoneNumber },
         { $set: req.body }
     );
+    resp.send(result);
+});
+
+app.delete("/:id", async (req, resp) => {
+    console.log(req.params.id);
+    const data = await dbConnect();
+    const result = await data.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
     resp.send(result);
 });
 app.listen(5000);
