7270508bcc3d74486187261fb04f4caaa5e562c7
Saurav Kumar <kumarsaurav26198@gmail.com>
2022-09-05 13:26:36 +0530

    updateworking

---
+++ api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
+
+app.use(express.json());
+app.get('/', async (req, resp) => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data);
+    resp.send(data);
+
+});
+app.post("/", async (req, resp) => {
+    let data = await dbConnect();
+    let result = await data.insert(req.body);
+    resp.send(result);
+
+});
+app.put("/:phoneNumber", async (req, resp) => {
+    console.log(req.body);
+    const data = await dbConnect();
+    let result = data.updateOne(
+        { phoneNumber: req.params.phoneNumber },
+        { $set: req.body }
+    );
+    resp.send(result);
+});
+app.listen(5000);
